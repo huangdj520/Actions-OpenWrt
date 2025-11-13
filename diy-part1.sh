601289b836000c52dea7c8e08a178b1eb29197ca
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2025-11-13 20:54:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,17 +11,16 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-echo 'src-git modem https://github.com/FUjr/modem_feeds.git;main' >> feeds.conf.default
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-# 自定义第三方插件
-#git clone https://github.com/ximiTech/luci-app-msd_lite.git package/lean/luci-app-msd_lite
-#git clone https://github.com/DongyangHu/luci-app-poweroff.git package/lean/luci-app-poweroff
-#git clone https://github.com/neophack/luci-app-ssr-plus.git package/lean/luci-app-ssr-plus
-#git clone https://github.com/chenmozhijin/luci-app-socat.git package/lean/luci-app-socat
-git clone https://github.com/momokind/luci-app-hypermodem.git package/lean/luci-app-hypermodem
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
+# echo 'src-git modemfeed https://github.com/koshev-msk/modemfeed' >>feeds.conf.default
+# echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
+# echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+git clone https://github.com/lkiuyu/luci-app-cpu-perf package/luci-app-cpu-perf
+git clone https://github.com/lkiuyu/luci-app-cpu-status package/luci-app-cpu-status
+git clone https://github.com/lkiuyu/luci-app-temp-status package/luci-app-temp-status
+git clone https://github.com/lkiuyu/DbusSmsForwardCPlus package/DbusSmsForwardCPlus
